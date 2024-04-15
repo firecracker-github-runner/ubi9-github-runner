5951fa0f2909b1203696eb48ad8fad589ea325c1
David Skiff <dskiff@users.noreply.github.com>
2024-04-14 20:38:24 -0400

    cut unused

--- sudoShim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# A shim for sudo that just executes using the original username
-# This is a hack for some github actions that use sudo internally
-# NOTE: If sudo is actually needed, it will still obviously fail
-echo "WARNING: invoking command as original user (NO SUDO)"
-exec "$@"
